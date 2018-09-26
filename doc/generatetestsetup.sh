b8c542073ad5eab4002c2976008f0b58856056b8
James Hatheway <james.hatheway@gmail.com>
2018-09-26 16:49:03 -0700

    [issue2] generatetestsetup.sh script had a few errors that prevented it from working. This commit fixes these issues

--- doc/generatetestsetup.sh
+++ doc/generatetestsetup.sh
@@ -4,37 +4,38 @@ TGS="tg1 tg2 tg3 tg4"
 USERS="user1 user2 user3 user4"
 LISTS="warroom"
 
-CLI=cli/tcli.go
+CLI=./src/cmd/tcli/tcli.go
 
 echo "* Creating users..."
 for US in ${USERS}
 do
 	echo "- ${US}"
 	$CLI user new ${US} ${US}@example.org
-	$CLI user set fullname ${US} ${US}
-	$CLI user set telephone ${US} "+15552352352"
-	$CLI user set sms ${US} "+15552352352"
-	$CLI user set postal ${US} "Some Street, The City"
-	$CLI user set biography ${US} "So much to tell"
+	$CLI user set name_first ${US} ${US}
+	$CLI user set name_last ${US} ${US}
+	$CLI user set tel_info ${US} "+15552352352"
+	$CLI user set sms_info ${US} "+15552352352"
+	$CLI user set post_info ${US} "Some Street, The City"
+	$CLI user set bio_info ${US} "So much to tell"
 done
 
 echo "* Creating TrustGroups..."
 for TG in ${TGS};
 do
 	echo "- ${TG}"
-	$CLI tg add ${TG}
-	$CLI tg set groupdesc "Generated: ${TG}"
+	$CLI group add ${TG}
+	$CLI group set descr ${TG} "Generated: ${TG}"
 
 	for US in ${USERS}
 	do
 		echo " - adding user ${US}"
-		$CLI tg member add ${TG} ${US}
+		$CLI group member add ${TG} ${US}
 	done
 
 	for ML in ${LISTS}
 	do
 		echo " ~ adding mailinglist ${ML}"
-		$CLI ml add ${TG} ${ML}
+		$CLI ml new ${TG} ${ML}
 
 		for US in ${USERS}
 		do
